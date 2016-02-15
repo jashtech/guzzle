d8c879371b79b958ea9e961233d01e428227f67f
Michał Koliński <mkolinski@jash.pl>
2016-02-15 15:03:53 +0100

    Made Middleware.php compatible with PHP 5.3.

--- src/Middleware.php
+++ src/Middleware.php
@@ -24,7 +24,7 @@ final class Middleware
      */
     public static function cookies()
     {
-        return function (callable $handler) {
+        return function ($handler) {
             return function ($request, array $options) use ($handler) {
                 if (empty($options['cookies'])) {
                     return $handler($request, $options);
@@ -51,7 +51,7 @@ final class Middleware
      */
     public static function httpErrors()
     {
-        return function (callable $handler) {
+        return function ($handler) {
             return function ($request, array $options) use ($handler) {
                 if (empty($options['http_errors'])) {
                     return $handler($request, $options);
@@ -83,25 +83,25 @@ final class Middleware
             throw new \InvalidArgumentException('history container must be an array or object implementing ArrayAccess');
         }
 
-        return function (callable $handler) use (&$container) {
+        return function ($handler) use (&$container) {
             return function ($request, array $options) use ($handler, &$container) {
                 return $handler($request, $options)->then(
                     function ($value) use ($request, &$container, $options) {
-                        $container[] = [
+                        $container[] = array(
                             'request'  => $request,
                             'response' => $value,
                             'error'    => null,
                             'options'  => $options
-                        ];
+                        );
                         return $value;
                     },
                     function ($reason) use ($request, &$container, $options) {
-                        $container[] = [
+                        $container[] = array(
                             'request'  => $request,
                             'response' => null,
                             'error'    => $reason,
                             'options'  => $options
-                        ];
+                        );
                         return new RejectedPromise($reason);
                     }
                 );
@@ -122,9 +122,9 @@ final class Middleware
      *
      * @return callable Returns a function that accepts the next handler.
      */
-    public static function tap(callable $before = null, callable $after = null)
+    public static function tap($before = null, $after = null)
     {
-        return function (callable $handler) use ($before, $after) {
+        return function ($handler) use ($before, $after) {
             return function ($request, array $options) use ($handler, $before, $after) {
                 if ($before) {
                     $before($request, $options);
@@ -145,7 +145,7 @@ final class Middleware
      */
     public static function redirect()
     {
-        return function (callable $handler) {
+        return function ($handler) {
             return new RedirectMiddleware($handler);
         };
     }
@@ -165,9 +165,9 @@ final class Middleware
      *
      * @return callable Returns a function that accepts the next handler.
      */
-    public static function retry(callable $decider, callable $delay = null)
+    public static function retry($decider, $delay = null)
     {
-        return function (callable $handler) use ($decider, $delay) {
+        return function ($handler) use ($decider, $delay) {
             return new RetryMiddleware($decider, $handler, $delay);
         };
     }
@@ -184,7 +184,7 @@ final class Middleware
      */
     public static function log(LoggerInterface $logger, MessageFormatter $formatter, $logLevel = LogLevel::INFO)
     {
-        return function (callable $handler) use ($logger, $formatter, $logLevel) {
+        return function ($handler) use ($logger, $formatter, $logLevel) {
             return function ($request, array $options) use ($handler, $logger, $formatter, $logLevel) {
                 return $handler($request, $options)->then(
                     function ($response) use ($logger, $request, $formatter, $logLevel) {
@@ -213,7 +213,7 @@ final class Middleware
      */
     public static function prepareBody()
     {
-        return function (callable $handler) {
+        return function ($handler) {
             return new PrepareBodyMiddleware($handler);
         };
     }
@@ -226,9 +226,9 @@ final class Middleware
      *                     a RequestInterface.
      * @return callable
      */
-    public static function mapRequest(callable $fn)
+    public static function mapRequest($fn)
     {
-        return function (callable $handler) use ($fn) {
+        return function ($handler) use ($fn) {
             return function ($request, array $options) use ($handler, $fn) {
                 return $handler($fn($request), $options);
             };
@@ -243,9 +243,9 @@ final class Middleware
      *                     returns a ResponseInterface.
      * @return callable
      */
-    public static function mapResponse(callable $fn)
+    public static function mapResponse($fn)
     {
-        return function (callable $handler) use ($fn) {
+        return function ($handler) use ($fn) {
             return function ($request, array $options) use ($handler, $fn) {
                 return $handler($request, $options)->then($fn);
             };
