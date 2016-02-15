75e31ff6e686b225f7840bac74eef26f1dcb71eb
Michał Koliński <mkolinski@jash.pl>
2016-02-15 14:16:29 +0100

    Made StreamHandler.php compatible with PHP 5.3.

--- src/Handler/StreamHandler.php
+++ src/Handler/StreamHandler.php
@@ -17,7 +17,7 @@ use Psr\Http\Message\StreamInterface;
  */
 class StreamHandler
 {
-    private $lastHeaders = [];
+    private $lastHeaders = array();
 
     /**
      * Sends an HTTP request.
@@ -84,7 +84,7 @@ class StreamHandler
                 $response,
                 microtime(true) - $startTime,
                 $error,
-                []
+                array()
             );
             call_user_func($options['on_stats'], $stats);
         }
@@ -97,7 +97,7 @@ class StreamHandler
         $startTime
     ) {
         $hdrs = $this->lastHeaders;
-        $this->lastHeaders = [];
+        $this->lastHeaders = array();
         $parts = explode(' ', array_shift($hdrs), 3);
         $ver = explode('/', $parts[0])[1];
         $status = $parts[1];
@@ -168,7 +168,7 @@ class StreamHandler
             }
         }
 
-        return [$stream, $headers];
+        return array($stream, $headers);
     }
 
     /**
@@ -197,15 +197,15 @@ class StreamHandler
      * @return resource
      * @throws \RuntimeException on error
      */
-    private function createResource(callable $callback)
+    private function createResource($callback)
     {
         $errors = null;
         set_error_handler(function ($_, $msg, $file, $line) use (&$errors) {
-            $errors[] = [
+            $errors[] = array(
                 'message' => $msg,
                 'file'    => $file,
                 'line'    => $line
-            ];
+            );
             return true;
         });
 
@@ -245,7 +245,7 @@ class StreamHandler
             $options['verify'] = true;
         }
 
-        $params = [];
+        $params = array();
         $context = $this->getDefaultContext($request, $options);
 
         if (isset($options['on_headers']) && !is_callable($options['on_headers'])) {
@@ -295,15 +295,15 @@ class StreamHandler
             }
         }
 
-        $context = [
-            'http' => [
+        $context = array(
+            'http' => array(
                 'method'           => $request->getMethod(),
                 'header'           => $headers,
                 'protocol_version' => $request->getProtocolVersion(),
                 'ignore_errors'    => true,
                 'follow_location'  => 0,
-            ],
-        ];
+            ),
+        );
 
         $body = (string) $request->getBody();
 
@@ -402,7 +402,7 @@ class StreamHandler
             return;
         }
 
-        static $map = [
+        static $map = array(
             STREAM_NOTIFY_CONNECT       => 'CONNECT',
             STREAM_NOTIFY_AUTH_REQUIRED => 'AUTH_REQUIRED',
             STREAM_NOTIFY_AUTH_RESULT   => 'AUTH_RESULT',
@@ -413,9 +413,9 @@ class StreamHandler
             STREAM_NOTIFY_FAILURE       => 'FAILURE',
             STREAM_NOTIFY_COMPLETED     => 'COMPLETED',
             STREAM_NOTIFY_RESOLVE       => 'RESOLVE',
-        ];
-        static $args = ['severity', 'message', 'message_code',
-            'bytes_transferred', 'bytes_max'];
+        );
+        static $args = array('severity', 'message', 'message_code',
+            'bytes_transferred', 'bytes_max');
 
         $value = \GuzzleHttp\debug_resource($value);
         $ident = $request->getMethod() . ' ' . $request->getUri();
@@ -433,16 +433,16 @@ class StreamHandler
         );
     }
 
-    private function addNotification(array &$params, callable $notify)
+    private function addNotification(array &$params, $notify)
     {
         // Wrap the existing function if needed.
         if (!isset($params['notification'])) {
             $params['notification'] = $notify;
         } else {
-            $params['notification'] = $this->callArray([
+            $params['notification'] = $this->callArray(array(
                 $params['notification'],
                 $notify
-            ]);
+            ));
         }
     }
 
